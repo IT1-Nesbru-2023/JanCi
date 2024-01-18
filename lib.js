0d67119bd06fbc4aa29995f64496679dc2ec080d
janci1234 <145429401+janci1234@users.noreply.github.com>
2024-01-18 13:32:24 +0100

    Update lib.js

--- lib.js
+++ lib.js
@@ -2,9 +2,3 @@ function randomRange(min, max) {
     return (Math.round(Math.random()*(max-min))+min);
     console.log("Jan Cichowicz")
 };
-
-let t1 = Number(prompt("tall 1"));
-let t2 = Number(prompt("tall 2"));
-
-let result = randomRange(t1, t2);
-document.write(result);
